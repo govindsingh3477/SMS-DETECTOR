52212a5c7da1dbfd6a9788e605bc848b144a8582
govind singh gurjar <gurjargovind994@gmail.com>
2024-06-27 21:02:12 +0530

    modified app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ st.title("Email/SMS SPAM CLASSIFIER")
 
 input_sms = st.text_area("Enter your message")
 if st.button('Predict'):
-    transformed_sms=transform_text(input_sms);
-
+    transformed_sms=transform_text(input_sms)
     vector_input=tfidf.transform([transformed_sms])
     result = model.predict(vector_input)[0]
     if result==1:
