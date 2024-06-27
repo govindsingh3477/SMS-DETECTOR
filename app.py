fceb8e2c8b193031629f214b054c994bbf96279d
govind singh gurjar <gurjargovind994@gmail.com>
2024-06-27 18:52:36 +0530

    uploaded to remote

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import  stopwords
+import nltk
+from nltk.stem.porter import  PorterStemmer
+ps=PorterStemmer()
+tfidf=pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    text = y[:]
+    y.clear()
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+st.title("Email/SMS SPAM CLASSIFIER")
+
+input_sms = st.text_area("Enter your message")
+if st.button('Predict'):
+    transformed_sms=transform_text(input_sms);
+
+    vector_input=tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+    if result==1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
+
+
+
